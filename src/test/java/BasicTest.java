0d0524e3bd8cc4746fc833db979e209406c9cb5c
ghadagamal123 <ghadagamalelden@gmail.com>
2026-06-26 12:03:07 +0300

    commit

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class test {
+public class BasicTest {
     @Test
     public void test(){
         WebDriver driver = new ChromeDriver();
